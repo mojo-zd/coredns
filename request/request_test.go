8d1a3feb8a52c2a87b3fa7e00c5eeaf96c23dd89
Tobias Schmidt <tobidt@gmail.com>
2018-07-09 20:01:46 +0200

    Add benchmark for response truncation

--- request/request_test.go
+++ request/request_test.go
@@ -229,10 +229,30 @@ func BenchmarkRequestSize(b *testing.B) {
 	}
 }
 
+func BenchmarkRequestScrub(b *testing.B) {
+	st := testRequest()
+
+	reply := new(dns.Msg)
+	reply.SetReply(st.Req)
+	for i := 1; i < 33; i++ {
+		reply.Answer = append(reply.Answer, test.SRV(
+			fmt.Sprintf("large.example.com. 10 IN SRV 0 0 80 10-0-0-%d.default.pod.k8s.example.com.", i)))
+	}
+	for i := 1; i < 33; i++ {
+		reply.Extra = append(reply.Extra, test.A(
+			fmt.Sprintf("10-0-0-%d.default.pod.k8s.example.com. 10 IN A 10.0.0.%d", i, i)))
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		st.Scrub(reply.Copy())
+	}
+}
+
 func testRequest() Request {
 	m := new(dns.Msg)
 	m.SetQuestion("example.com.", dns.TypeA)
-	m.SetEdns0(4097, true)
+	m.SetEdns0(4096, true)
 	return Request{W: &test.ResponseWriter{}, Req: m}
 }
 
